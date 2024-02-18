b2048d9f85cecf13c4c31e0f7401da72e1045400
ATN20222 <antonabdalla30@gmail.com>
2024-02-18 15:08:48 +0200

    finish

--- main.js
+++ main.js
@@ -12,28 +12,12 @@ EnableVariationsBtn.addEventListener('click' , ()=>{
 
 
 
-
-var OptionsObj = {
-    options :[
-        {
-            OptionName: "colors",
-            Values : []
-        },
-        {
-            OptionName: "Sizes",
-            Values : []
-        }
-        
-        
-    ]
-}
 var AddVariationOption = document.getElementById("AddVariationOption");
 var OpationContainer = document.querySelector(".OpationContainer");
 
-var AddOptionBtn = document.querySelectorAll(".AddOptionBtn");
+var AddOptionBtn = document.querySelector(".AddOptionBtn");
 var OptionsRow = document.querySelector(".OptionsRow");
-
-
+var SingleOnly = true;
 
 
 var ColorCheckBox = document.querySelector(".ColorCheckBox");
@@ -41,29 +25,16 @@ var SizeCheckBox = document.querySelector(".SizeCheckBox");
 var ColorVar = document.querySelector(".ColorVar");
 var SizeVar = document.querySelector(".SizeVar");
 var SaveVariationsItems  = document.getElementById("SaveVariationsItems");
+
 var Colors=[];
 
+var LimitNumOfOptions = 2;
+var NumOfCurrentOptions = 1;
 
+AddOptionBtn.addEventListener('click',()=>{
 
-
-
-AddVariationOption.addEventListener('click', () => {
-    var ClonedOpationContainer = OpationContainer.cloneNode(true);
-
-        // console.log(ClonedOpationContainer.querySelector(".row .col-lg-12"));
-        
-        
-        OptionsRow.appendChild(ClonedOpationContainer);
-        OptionsRow.lastElementChild.querySelector(".row .col-lg-12 .OptionsValuesRow").lastChild.remove();
-        // console.log(OptionsRow.lastElementChild.querySelector(".row .col-lg-12 .OptionsValuesRow"));
-    // console.log(OptionsRow.querySelector(":last-child"));
-    
-    
-    AddOptionBtn = document.querySelectorAll(".AddOptionBtn");
-    
-    AddOptionBtn.forEach((AddOpBtn) => {
-        AddOpBtn.addEventListener('click' , () => {
-            var nextEle = AddOpBtn.closest("div");
+            
+            var nextEle = AddOptionBtn.parentElement;
             var PrevParent = nextEle.previousElementSibling;
             var PrevOpt = PrevParent.previousElementSibling;
             var PrvSelectedVar = PrevOpt.querySelector('.VarList').value;
@@ -72,18 +43,14 @@ AddVariationOption.addEventListener('click', () => {
             
             
 
-            // console.log(PrvSelectedVar); 
-            // console.log(PrevInputValue); 
-
             var OptionsValuesRow = nextEle.nextElementSibling.querySelector(".row");
-            // console.log(OptionsValuesRow);
             
                 if(PrevInputValue.value.split(' ').join('')!=""&&Colors.includes(PrevInputValue.value)==false){
                     OptionsValuesRow.innerHTML += `<div class="col-lg-2 SizeItem">
                     <input type="text" disabled value="${PrevInputValue.value}">
                     <button class="btn RemoveBtn">x</button>
                 </div>`
-                
+                PrevInputValue.value="";
                     Colors.push(PrevInputValue.value);
                 }
                 
@@ -97,176 +64,240 @@ AddVariationOption.addEventListener('click', () => {
                             const indexToRemove = Colors.indexOf(parentColorItem.querySelector('input').value);
                             if (indexToRemove !== -1) {
                                 Colors.splice(indexToRemove, 1);
-                                // console.log(Colors);
                             }
                         }
                     }
                 });
-                // console.log(Colors);
-
-
-            //     console.log(Colors);
             
 
         });
+
+
+
+
+    AddVariationOption.addEventListener('click', () => {
+        SingleOnly=false;
+        if(NumOfCurrentOptions< LimitNumOfOptions){
+
+        
+
+        NumOfCurrentOptions++;
+        var ClonedOpationContainer = OpationContainer.cloneNode(true);
+    
+            OptionsRow.appendChild(ClonedOpationContainer);
+            var toRemove = OptionsRow.lastElementChild.querySelectorAll(".row .col-lg-12 .OptionsValuesRow .SizeItem");
+            toRemove.forEach((e)=>{
+                e.remove(); 
+            });
+            // console.log(OptionsRow.lastElementChild.querySelector(".row .col-lg-12 .OptionsValuesRow").lastChild);
+        AddOptionBtn = document.querySelectorAll(".AddOptionBtn");
+        
+        AddOptionBtn.forEach((AddOpBtn) => {
+            AddOpBtn.addEventListener('click' , () => {
+                var nextEle = AddOpBtn.closest("div");
+                var PrevParent = nextEle.previousElementSibling;
+                var PrevOpt = PrevParent.previousElementSibling;
+                var PrvSelectedVar = PrevOpt.querySelector('.VarList').value;
+                
+                var PrevInputValue = PrevParent.querySelector('.OptionInput');
+                
+                
+    
+                var OptionsValuesRow = nextEle.nextElementSibling.querySelector(".row");
+                
+                    if(PrevInputValue.value.split(' ').join('')!=""&&Colors.includes(PrevInputValue.value)==false){
+                        OptionsValuesRow.innerHTML += `<div class="col-lg-2 SizeItem">
+                        <input type="text" disabled value="${PrevInputValue.value}">
+                        <button class="btn RemoveBtn">x</button>
+                    </div>`
+                    PrevInputValue.value="";
+                        Colors.push(PrevInputValue.value);
+                    }
+                    
+    
+                    
+                    OptionsValuesRow.addEventListener('click', (event) => {
+                        if (event.target.classList.contains('RemoveBtn')) {
+                            const parentColorItem = event.target.closest('.col-lg-2');
+                            if (parentColorItem) {
+                                parentColorItem.remove();
+                                const indexToRemove = Colors.indexOf(parentColorItem.querySelector('input').value);
+                                if (indexToRemove !== -1) {
+                                    Colors.splice(indexToRemove, 1);
+                                }
+                            }
+                        }
+                    });
+                
+    
+            });
+    
+        });
+
+
+    }});
+     
+    
+
+
+
+// function AddOptHandler(AddOpBtn){
+//     console.log(AddOpBtn);
+// }
+
+
+
+var OptionsObj = {
+    opts :[],
+}
+
+var Saved = false;
+var SaveVariations = document.getElementById("SaveVariations");
+var table = document.getElementById("Table");
+SaveVariations.addEventListener('click' , ()=>{
+    if(Saved==false){
+        var OpationContainer = document.querySelectorAll(".OpationContainer");
+        OpationContainer.forEach((ele)=>{
+            var opt = {
+                id :"",
+                Values:[],
+            }
+            var id = ele.querySelector(".VarList").value;
+            var items=[];
+            var Options = ele.querySelectorAll(".SizeItem input");
+            Options.forEach((e)=>{
+                items.push(e.value);
+            });
+            opt.id=id;
+            opt.Values= items;
+            OptionsObj.opts.push(opt);
+        });
+    
+        var Single = false;
+        var combinations = [];
+        if(OptionsObj.opts[0].Values.length==0){
+            OptionsObj.opts[1].Values.forEach((e)=>{
+                combinations.push(`${OptionsObj.opts[1].id+"/"+e}`);
+            });
+    
+            Single = true;
+    
+        }
+    
+            // if(OptionsObj.opts[1].Values.length == 0){
+    
+            //     OptionsObj.opts[0].Values.forEach((e)=>{
+            //         combinations.push(`${OptionsObj.opts[0].id+"/"+e}`);
+            //     });        
+                
+            //     Single = true;
+            // }
+     
+         
+        else{
+            
+            generateCombinationsWithIds(OptionsObj.opts, 0, [], combinations);
+            console.log(combinations);
+        }
+    
+    
+    combinations.forEach((ele) => {
+        var parts = ele.split('/');
+        var firstPart = parts[0];
+        var secondPart = parts[2];
+        var hiddenInputId = `${firstPart}/${secondPart}`;
+        var labelValue = `${parts[1]}/${parts[3]}`;
+        if(Single == true || SingleOnly==true ){
+            console.log("idoit");
+            labelValue =parts[1];
+            hiddenInputId = firstPart;
+        }
+    
+
+        if (!document.getElementById(`${ele}tr`)) {
+            table.innerHTML += `<tr class="TableRow Center Flex" id="${labelValue}tr" style="padding:10px;">
+                <td  scope="col" style="padding:20px"> 
+                    <label class="ProductVariantName" for="${labelValue}" name="${labelValue}">${labelValue}</label>
+                    <input class="ProductVariantId" type="hidden" value="${hiddenInputId}">
+                </td>
+                <td  scope="col" style="padding:20px"> 
+                    <input class="ProductVariantPrice form-control" type="text">
+                </td>
+                
+                <td class="d-flex" scope="col" style="padding:20px">
+                    <input id="${labelValue+'SKU'}" type="text" class="form-control ProductVariantSku">
+                </td>
+            </tr>`;
+        }
+    });
+
+    var OpationContainer = document.querySelectorAll(".OpationContainer");
+    OpationContainer.forEach((e)=>{
+        var inputs=e.querySelectorAll("input");
+        var SelectList = e.querySelector("select");
+        var btns = e.querySelectorAll("button");
+        inputs.forEach((i)=>{
+            i.setAttribute("disabled" , "true");
+        });
+        btns.forEach((btn)=>{
+            btn.disabled= true;
+        });
+        SelectList.setAttribute("disabled" ,"true");
+        AddVariationOption.disabled=true;
+        SaveVariations.disabled= true;
 
     });
+    Saved = true;
+    }   
+    SaveVariations.innerText="Saved";
+
 });
- 
 
+function generateCombinationsWithIds(options, index, currentCombination, result) {
+    if (index === options.length) {
+        result.push(currentCombination.join('/'));
+        return;
+    }
 
-function AddOptHandler(AddOpBtn){
-    console.log(AddOpBtn);
+    const currentOption = options[index];
+
+    for (const value of currentOption.Values) {
+        currentCombination.push(`${currentOption.id}/${value}`);
+        generateCombinationsWithIds(options, index + 1, currentCombination, result);
+        currentCombination.pop();
+    }
 }
 
 
-// AddOptionBtn.forEach((AddOpBtn) => {
-//     console.log("AddOpBtn");
-// });
+var SubProducts = {
+    ProductItem :[],
+}
 
 
-
-
-
-
-
-
-
-
-
-// window.onload = () => {
-//     if(SizeCheckBox.attributes.getNamedItem("Selected")){
-//         SizeVar.classList.add("Show");
-//         SaveVariationsItems.classList.add("Show");
-//         // console.log(SizeVar.classList);
-//     }
-//     if(ColorCheckBox.attributes.getNamedItem("Selected")){
-//         ColorVar.classList.add("Show");
-//         SaveVariationsItems.classList.add("Show");
-//         // console.log(SizeVar.classList);
-//     }
+var SaveProducts = document.getElementById("SaveProducts");
+SaveProducts.addEventListener('click' , ()=>{
+    var TableRows = document.querySelectorAll(".TableRow");
+    TableRows.forEach((ele)=>{
+        // id name price sku
         
-        
-//   };
 
-var table = document.getElementById("Table");
-var ColorInput = document.getElementById("ColorInput");
-var AddColorBtn = document.getElementById("AddColorBtn");
-var ColorRow = document.getElementById("ColorRow");
-
-var Colors = [];
-
-AddColorBtn.addEventListener('click', () => {
-    if(ColorInput.value.split(' ').join('')!=""&&Colors.includes(ColorInput.value)==false){
-        ColorRow.innerHTML += `<div class="col-lg-2 SizeItem">
-        <input type="text" disabled value="${ColorInput.value}">
-        <button class="btn RemoveBtn">x</button>
-    </div>`
-    
-    Colors.push(ColorInput.value);
-    }
-
-
-    console.log(Colors);
-});
-
-ColorRow.addEventListener('click', (event) => {
-    if (event.target.classList.contains('RemoveBtn')) {
-        const parentColorItem = event.target.closest('.col-lg-2');
-        if (parentColorItem) {
-            parentColorItem.remove();
-            const indexToRemove = Colors.indexOf(parentColorItem.querySelector('input').value);
-            if (indexToRemove !== -1) {
-                Colors.splice(indexToRemove, 1);
-                // console.log(Colors);
+        var PVarId = ele.querySelector(".ProductVariantId").value;
+        var PVarName= ele.querySelector(".ProductVariantName").textContent;
+        var PVarPrice= ele.querySelector(".ProductVariantPrice").value;
+        var PVarSku= ele.querySelector(".ProductVariantSku").value;
+        var PItem = {
+            ProductVariantId :PVarId,
+            ProductVariantName:PVarName,
+            ProductVariantPrice:PVarPrice,
+            ProductVariantSku:PVarSku, 
             }
-        }
-    }
-});
-
-
-
-var Sizes = [];
-var SizeInput = document.getElementById("SizeInput");
-var AddSizeBtn = document.getElementById("AddSizeBtn");
-var SizeRow = document.getElementById("SizeRow");
-
-// AddSizeBtn.addEventListener('click', () => {
-//     console.log();
-//     if(SizeInput.value.split(' ').join('')  !=""&&Sizes.includes(SizeInput.value)==false){
-//             SizeRow.innerHTML += `<div class="col-lg-2 SizeItem">
-//             <input type="text" disabled value="${SizeInput.value}">
-//             <button class="btn RemoveBtn">x</button>
-//         </div>`
-
-//         Sizes.push(SizeInput.value);
-//     }
-
-//     console.log(Sizes);
-// });
-
-// SizeRow.addEventListener('click', (event) => {
-//     if (event.target.classList.contains('RemoveBtn')) {
-//         const parentSizeItem = event.target.closest('.SizeItem');
-//         if (parentSizeItem) {
-//             parentSizeItem.remove();
-
-//             const indexToRemove = Sizes.indexOf(parentSizeItem.querySelector('input').value);
-//             if (indexToRemove !== -1) {
-//                 Sizes.splice(indexToRemove, 1);
-//                 // console.log(Sizes);
-//             }
-//         }
-//     }
-// });
-
-
-var SaveVariationsItems = document.getElementById("SaveVariationsItems");
-
-SaveVariationsItems.addEventListener( 'click' , ()=>{
-let combinations = [];
-
-    if(Sizes.length==0 && Colors.length!=0){
-        Colors.forEach((item2) => {
-            if(!combinations.includes(item2)) 
-                combinations.push(item2);
-        });
-        console.log(Colors);
         
-    }else if(Sizes.length!=0 && Colors.length==0){
-        Sizes.forEach((item1) => {
-        if(!combinations.includes(item1)) 
-            combinations.push(item1);
-        
-        });
-    }else{
-        Sizes.forEach((item1) => {
-        Colors.forEach((item2) => {
-            const comb = item1 + '/' + item2;
-            if(!combinations.includes(comb)) 
-                combinations.push(comb);
-        });
-        });
+        SubProducts.ProductItem.push(PItem);
+
     
-}
 
-console.log(combinations);
-combinations.forEach((ele)=>{
-    ;
-    if (!document.getElementById(`${ele}tr`)) {
-        table.innerHTML += `<tr class="TableRow Center Flex" id="${ele}tr" style="padding:10px;">
-            <td  scope="col" style="padding:20px"> 
-                <label for="${ele}" name="${ele}">${ele}</label>
-                <input type="hidden" value="${ele}">
-            </td>
-            <td class="d-flex" scope="col" style="padding:20px">
-            <label for="${ele+'SKU'}" name="${ele+'SKU'}"></label>
-                <input id="${ele+'SKU'}" type="text" class="form-control">
-            </td>
-            
-        </tr>`;
-}
+    });
+    console.log(SubProducts);
 });
 
 
-});
